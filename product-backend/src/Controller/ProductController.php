731a4479e82954ec89e6c995b0f66b43b2c567bb
Ali Ibrahim <aliibrahim@Macbook-4.local>
2025-04-29 13:31:24 +0200

    Initial commit

---
+++ product-backend/src/Controller/ProductController.php
@@ -0,0 +1,84 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Service\ProductStorageService;
+
+class ProductController extends AbstractController
+{
+    private ProductStorageService $storage;
+
+    public function __construct(ProductStorageService $storage)
+    {
+        $this->storage = $storage;
+    }
+
+    #[Route('/products', methods: ['POST'])]
+    public function create(Request $request): Response
+    {
+        $data = json_decode($request->getContent(), true);
+
+        if (!$data) {
+            return $this->json(['error' => 'Invalid JSON'], Response::HTTP_BAD_REQUEST);
+        }
+
+        $products = $this->storage->getAll();
+        $data['id'] = count($products) ? max(array_column($products, 'id')) + 1 : 1;
+        $data['createdAt'] = time();
+        $data['updatedAt'] = time();
+
+        $this->storage->add($data);
+
+        return $this->json($data, Response::HTTP_CREATED);
+    }
+
+    #[Route('/products', methods: ['GET'])]
+    public function getAll(): Response
+    {
+        return $this->json($this->storage->getAll());
+    }
+
+    #[Route('/products/{id}', methods: ['GET'])]
+    public function getOne(int $id): Response
+    {
+        $product = $this->storage->getById($id);
+
+        if (!$product) {
+            return $this->json(['error' => 'Product not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        return $this->json($product);
+    }
+
+    #[Route('/products/{id}', methods: ['PATCH'])]
+    public function update(Request $request, int $id): Response
+    {
+        $data = json_decode($request->getContent(), true);
+
+        if (!$data) {
+            return $this->json(['error' => 'Invalid JSON'], Response::HTTP_BAD_REQUEST);
+        }
+
+        $updated = $this->storage->update($id, array_merge($data, ['updatedAt' => time()]));
+
+        if (!$updated) {
+            return $this->json(['error' => 'Product not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        return $this->json(['message' => 'Product updated']);
+    }
+
+    #[Route('/products/{id}', methods: ['DELETE'])]
+    public function delete(int $id): Response
+    {
+        $deleted = $this->storage->delete($id);
+
+        if (!$deleted) {
+            return $this->json(['error' => 'Product not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        return $this->json(['message' => 'Product deleted']);
+    }
+}
---
+++ product-backend/src/Service/ProductStorageService.php
@@ -0,0 +1,71 @@
+<?php
+
+
+use ..\..\src\Entity\Product;
+
+class ProductStorageService
+{
+    private string $file;
+
+    public function __construct()
+    {
+        $this->file = __DIR__ . '/../../data/products.json';
+        if (!file_exists($this->file)) {
+            file_put_contents($this->file, json_encode([]));
+        }
+    }
+
+    public function getAll(): array
+    {
+        $data = json_decode(file_get_contents($this->file), true);
+        return $data ?? [];
+    }
+
+    public function getById(int $id): ?array
+    {
+        foreach ($this->getAll() as $product) {
+            if ($product['id'] === $id) {
+                return $product;
+            }
+        }
+        return null;
+    }
+
+    public function save(array $products): void
+    {
+        file_put_contents($this->file, json_encode($products, JSON_PRETTY_PRINT));
+    }
+
+    public function add(array $product): void
+    {
+        $products = $this->getAll();
+        $products[] = $product;
+        $this->save($products);
+    }
+
+    public function update(int $id, array $newData): bool
+    {
+        $products = $this->getAll();
+        foreach ($products as &$product) {
+            if ($product['id'] === $id) {
+                $product = array_merge($product, $newData);
+                $this->save($products);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function delete(int $id): bool
+    {
+        $products = $this->getAll();
+        foreach ($products as $index => $product) {
+            if ($product['id'] === $id) {
+                array_splice($products, $index, 1);
+                $this->save($products);
+                return true;
+            }
+        }
+        return false;
+    }
+}
